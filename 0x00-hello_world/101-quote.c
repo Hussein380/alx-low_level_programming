4464906da66de7143fa66a468f841c20d00bc58e
Hussein380 <huznigsrsne@gmail.com>
2023-10-05 11:43:27 -0700

    unix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 #include <unistd.h>
+
 /**
  * main - execution starting point
- * Return: return 1 when successful
+ *
+ * Return: returns 1 when successful
  */
+
 int main(void)
 {
-	write(2, "and that piece of art is useful - Dora Korpar, 2015 - 10 -  19\n", 61);
+	write(2,
+	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
