68a173d73de8823573c60b931e18607ea52c2153
Hussein380 <huznigsrsne@gmail.com>
2023-10-06 08:11:26 -0700

    char

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: returns 0 when succesful
+ */
+int main(void)
+{
+	char i;
+for (i = 'a'; i <= 'z'; i++)
+	putchar('a');
+putchar('\n');
+return (0);
+}
