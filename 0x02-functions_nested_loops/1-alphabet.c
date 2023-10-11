4b71bf4fa838632675adf66e53b85de69a78d013
Hussein380 <huznigsrsne@gmail.com>
2023-10-11 03:47:36 -0700

    small alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet - execition pf programme start here
+ *
+ * Return: 0  returns 0
+ */
+
+void print_alphabet(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		_putchar(a);
+	}
+	_putchar('n');
+}
