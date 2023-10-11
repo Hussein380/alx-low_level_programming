750587a49a9d42d2559e42fe182babae8811e420
Hussein380 <huznigsrsne@gmail.com>
2023-10-11 05:15:06 -0700

    checks lower case x-ter

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _islower - checks for a lowecase character
+ *@c:  parameter to be checked
+ *Return: returns 1 or 0
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a' && c >= 'z')
+	{
+		return (1);
+	}
+
+	else
+	{
+		return (0);
+	}
+}
