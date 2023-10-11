9102ba0bb211aeedc5c0e9810864498b652894ec
Hussein380 <huznigsrsne@gmail.com>
2023-10-11 13:52:49 -0700

    lowercase

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -4,16 +4,10 @@
  *@c:  parameter to be checked
  *Return: returns 1 or 0
  */
-
 int _islower(int c)
 {
-	if (c >= 'a' && c >= 'z')
-
+	if (c >= 'a' && c <= 'z')
 		return (1);
-
-
 	else
-
 		return (0);
-
 }
