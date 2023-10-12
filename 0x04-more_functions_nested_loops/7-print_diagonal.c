1ae919ca40b744e66c8201136f43b4c1f05b9108
Hussein380 <huznigsrsne@gmail.com>
2023-10-12 11:45:03 -0700

    diaganol line on the terminal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -13,12 +13,16 @@ void print_diagonal(int n)
 		int i;
 		int j;
 
-		for (i = 1; i <= n; i++)
+		for (i = 0; i < n; i++)
 		{
-			for (j = 0; j <= i; j++)
-				_putchar(' ');
-		_putchar('\\');
-		_putchar('\n');
+			for (j = 0; j < n; j++)
+			{
+				if (j == i)
+					_putchar('\\');
+				else if (j < i)
+					_putchar(' ');
+			}
+			_putchar('\n');
 		}
 	}
 }
