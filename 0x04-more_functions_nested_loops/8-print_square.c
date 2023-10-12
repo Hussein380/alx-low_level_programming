ff1d9ecb78c35e499cb386836a82ecaab054737f
Hussein380 <huznigsrsne@gmail.com>
2023-10-12 06:50:24 -0700

    squares

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_square -  prints square
+ * @size: stores size of square
+ * Return: returns aquare
+ */
+void print_square(int size)
+{
+	if (size <= 0)
+		_putchar('\n');
+	else
+	{
+		int i, j;
+
+		for (i = 1; i <= size; i++)
+		{
+			for (j = 1; j <= size; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
