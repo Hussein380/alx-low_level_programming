e87cb5442aa2cfa655e6021f2dc1f2a84392154b
Hussein380 <huznigsrsne@gmail.com>
2023-10-18 04:44:35 -0700

    comparing strings

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,7 +1,9 @@
 #ifndef HEADER_H
 #define HEADER_H
 #include <string.h>
+#include <stdio.h>
 char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
 char *_strcat(char *dest, char *src);
 int _putchar(char c);
 char *_strncat(char *dest, char *src, int n);
