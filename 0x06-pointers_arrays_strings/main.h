dd9e2da624b7764c1bee52b50d73b594bb3e2c0d
Hussein380 <huznigsrsne@gmail.com>
2023-10-23 00:35:55 -0700

    fills memory with constant byte

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -13,4 +13,5 @@ char *_strcat(char *dest, char *src);
 int _putchar(char c);
 char *_strncat(char *dest, char *src, int n);
 void print_number(int n);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
 #endif
