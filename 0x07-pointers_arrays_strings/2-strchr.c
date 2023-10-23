8f4fb6548b513d78a8680efb859fffb6e3106a60
Hussein380 <huznigsrsne@gmail.com>
2023-10-23 10:06:09 -0700

    locate a character of string

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stddef.h>
 /**
  * _strchr - loccates a character in astring
  * @s: string
@@ -16,5 +17,5 @@ char *_strchr(char *s, char c)
 			return (&s[i]);
 		}
 	}
-	return (0);
+	return (NULL);
 }
