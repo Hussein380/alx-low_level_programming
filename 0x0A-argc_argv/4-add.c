b4a96770dd3ab4245fb02bb8d9b57d21c374bd9c
Hussein380 <huznigsrsne@gmail.com>
2023-10-27 10:17:56 -0700

    adds prime numbers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+/**
+ * main - prints prime num
+ * @argc: argument counts
+ * @argv: argument vector
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int i, num;
+	int sum = 0;
+	bool hasError = false;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	for (i = 1; i < argc; i++)
+	{
+		num = atoi(argv[i]);
+		if (num <= 0)
+		{
+			hasError = true;
+			break;
+		}
+		sum += num;
+	}
+	if (hasError)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+
+		printf("%d\n", sum);
+		return (0);
+	}
+}
