31727a948e362f3e94ec1a0eeca72bd8720acafd
Hussein380 <huznigsrsne@gmail.com>
2023-11-06 03:53:39 -0800

    added PREPROCESSOR DIRECTIVE

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,15 @@
-struct dog 
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - dof function
+ * @name: name of dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ */
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+#endif
