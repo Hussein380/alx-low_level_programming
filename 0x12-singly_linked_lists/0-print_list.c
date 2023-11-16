cb4a6cad2ec364047beb448e12fbdb5687e63f9b
Hussein380 <huznigsrsne@gmail.com>
2023-11-16 01:41:45 -0800

    prints element

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+#include <stddef.h>
+#include  <stdlib.h>
+#include <stdio.h>
+/**
+ *  print_list - prints all elements of a list
+ *  @h: head of the list
+ *  Return: number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	/* sets new to point the head*/
+	const list_t *new = h;
+	size_t count = 0;
+
+	while (new != NULL)
+	{
+		if (new->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", new->len, new->str);
+		count++;
+		new = new->next;
+	}
+	return (count);
+}
