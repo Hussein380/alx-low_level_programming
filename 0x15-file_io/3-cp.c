fc828f285408479f84aad46a665bc5aa2d918184
Hussein380 <huznigsrsne@gmail.com>
2023-11-27 23:04:53 -0800

    changed the syntax of the code

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,10 +1,22 @@
 #include <stdio.h>
+#include <sys/stat.h>
 #include <fcntl.h>
 #include <stdlib.h>
 #include <errno.h>
 #include <unistd.h>
 #define BUFFER_SIZE 1024
 
+/**
+ * error_exit - function to exit when error
+ * @code: code to exit with
+ * @message: pointer to message to exit with
+ * @arg: argument
+ */
+void error_exit(int code, const char *message, const char *arg)
+{
+	dprintf(STDERR_FILENO, message, arg);
+	exit(code);
+}
 /**
  * main - main function
  * @argc: argument count
@@ -19,35 +31,34 @@ int main(int argc, char *argv[])
 
 	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "usage: cp file_from file_to\n ");
-		exit(97);
+		error_exit(97, "usage: %s file_from file_to\n ", argv[0]);
 	}
 	file_from = open(argv[1], O_RDONLY);
 	if (file_from == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
+		error_exit(98, "Error: Can't read from file %s\n", argv[1]);
 	}
 	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0644);
 	if (file_to == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
+		error_exit(99, "Error: Can't write to %s\n", argv[2]);
 	}
 	while ((bytes_read = read(file_from, buffer, BUFFER_SIZE)) > 0)
 	{
 		bytes_written = write(file_to, buffer, bytes_read);
-		if (bytes_written == -1)
+		if (bytes_written != bytes_read)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-			exit(99);
+			error_exit(99, "Error: Can't write to %s\n", argv[2]);
 		}
+	}
+	if (bytes_read == -1)
+	{
+		error_exit(98, "Error: Can't read from file %s\n", argv[2]);
 	}
 	if (close(file_from) == -1 || close(file_to) == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n",
-				close(file_from) == -1 ? file_from : file_from);
-		exit(100);
+		error_exit(100, "Error: Can't close fd %d\n",
+				close(file_from) == -1 ? argv[1] : argv[2]);
 	}
 	return (0);
 }
