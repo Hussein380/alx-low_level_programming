0db55c210519667b4c8bac182d60ec79bf6fb64c
Hussein380 <huznigarane@gmail.com>
2024-01-11 06:48:29 -0800

    corrected bettystyle documentation

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -21,5 +21,5 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 	{
 		current = current->next;
 	}
-	return (i == index) ? current : NULL;
+	return ((i == index) ? current : NULL);
 }
