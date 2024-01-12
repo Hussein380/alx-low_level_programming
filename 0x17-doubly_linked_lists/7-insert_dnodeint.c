a257d689ce3622808d449b1af74fc12de72d0b68
Hussein380 <huznigarane@gmail.com>
2024-01-11 21:31:42 -0800

    changed the approach of the logic

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -9,43 +9,37 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *new_node, *current;
-	unsigned int i;
+	dlistint_t *new_node = malloc(sizeof(dlistint_t));
+	dlistint_t *tmp = *h;
+	unsigned int count = 0;
 
-	if (h == NULL)
-		return (NULL);
-	current = *h;
-
-	for (i = 0; i < idx - 1 && current != NULL; i++)
-		current = current->next;
-	if (idx != 0 && current == NULL)
-		return (NULL);
-	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
 		return (NULL);
+
 	new_node->n = n;
+	new_node->prev = NULL;
+	new_node->next = NULL;
 
 	if (idx == 0)
+		return (add_dnodeint(h, n));
+
+	while (tmp != NULL && count < idx - 1)
 	{
-		new_node->next = *h;
-		new_node->prev = NULL;
-
-		if (*h != NULL)
-		{
-			(*h)->prev = new_node;
-
-			*h = new_node;
-		}
+		tmp = tmp->next;
+		count++;
 	}
-	else
+	if (tmp == NULL)
 	{
-		new_node->next = current->next;
-		new_node->prev = current;
-
-		if (current->next != NULL)
-			current->next->prev = new_node;
-
-		current->next = new_node;
+		free(new_node);
+		return(NULL);
 	}
+	if (tmp->next == NULL)
+	{
+		return (add_dnodeint_end(h, n));
+	}
+	new_node->next = tmp->next;
+	new_node->prev = tmp;
+	tmp->next->prev = new_node;
+	tmp->next = new_node;
 	return (new_node);
 }
