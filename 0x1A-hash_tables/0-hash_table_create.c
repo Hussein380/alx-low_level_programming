319711d8430ebb2f16d40f08bc852f643e8f7da5
Hussein380 <huznigarane@gmail.com>
2024-02-03 00:02:00 -0800

    corrected the code by checking if size == 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -13,6 +13,9 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *new_table = NULL;
 	unsigned long int i;
 
+	if (size == 0)
+		return (NULL);
+
 	/*Allocate memory for the hash table structure*/
 	new_table = malloc(sizeof(hash_table_t));
 	if (new_table == NULL)
