91b98454eeb58c13af68f755ac12e0d67a33dcde
Hussein380 <huznigarane@gmail.com>
2024-05-08 09:47:43 +0300

    Implemented jump search algorithm

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -1,55 +1,56 @@
 #include <stdio.h>
-#include "search_algos.h"
 #include <math.h>
+#include "search_algos.h"
 
 /**
- * jump_search -  function that searches for a value in a sorted array
- * @array:  is a pointer to the first element of the array to search in
- * @size:  is the number of elements in array
- * @value:  is the value to search for
- * Return: the index of the vaklue if found or -1 if not found
+ * jump_search -  Searches for a value in a sorted array
+ * @array: Pointer to the first elements in the array
+ * @value: the value to search fo
+ * @size: size of the arrayr
+ * Return: The index of the value if found, otherwise -1
+ *
  */
 
 int jump_search(int *array, size_t size, int value)
 {
-	size_t jump = sqrt(size), i;
-	size_t left = 0, right = jump;
-	/*Check if the array is NULL or empty. If so, return -1.*/
+	size_t jump = sqrt(size);
+	size_t left = 0, right = jump, i;
+
 	if (array == NULL || size == 0)
 		return (-1);
-	/* Calculate the jump step as the square root of the size of the array.*/
-	jump = sqrt(size);
-	/* Loop while right is less than the size of the array:*/
+	/*loop while right is less than size  of the array*/
 	while (right < size)
 	{
-		/*Print the value checked during each iteration*/
-		printf("Value checked array[%lu] = [%d]\n", right, array[right]);
-		/*if array, right is  greater than or equal to target value*/
+		/*print the value checked during each iteration*/
+		printf("Value checked array[%lu] = [%d]\n", right,
+				array[right]);
+		/*if the value ar right index is greater than */
 		if (array[right] >= value)
 		{
-			/*perform linear search within the block defined by*/
+			/*perform linear search  within the block */
 			for (i = left; i < right; i++)
 			{
-				/*prin the value checked during iteration*/
-				printf("Value checked array[%lu] = [%d]\n",
-						i, array[i]);
-				/*if array is equal to target return i*/
+				printf("Value checked array[%lu] = [%d]\n", i,
+						array[i]);
+				/*return its index if the crnt matches trgt*/
 				if (array[i] == value)
 					return (i);
 			}
-			/*if the target value is not found within*/
+			/*if target  value not found within block*/
 			return (-1);
 		}
-		/*update prev to left and left to right*/
+		/*up[date left and right for the next jump*/
 		left = right;
 		right += jump;
 	}
-	/*perform linear search for the last block*/
+	/*perform linear search  for the last block*/
 	for (i = left; i < size; i++)
 	{
 		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+		/*if the current element matches the target , return its idx*/
 		if (array[i] == value)
 			return (i);
 	}
+	/*if the targt value is not found  in entire array, return -1*/
 	return (-1);
 }
